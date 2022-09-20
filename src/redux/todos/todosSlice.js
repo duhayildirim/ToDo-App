d79ea662efff5fd1318b8aac77de958ed85227e0
Duha YILDIRIM <1duhayildirim@gmail.com>
2022-09-21 02:41:07 +0300

    filtered

--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -3,7 +3,13 @@ import { createSlice } from "@reduxjs/toolkit"
 export const todosSlice = createSlice({
   name: 'todos',
   initialState: {
-    items: []
+    items: [
+      { id: 1, title: 'learning react', completed: true },
+      { id: 2, title: 'go to gym', completed: true },
+      { id: 3, title: 'math exercise', completed: false },
+      { id: 4, title: 'drink water', completed: false }
+    ],
+    activeFilter: 'all'
   },
   reducers: {
     addTodo: (state, action) => {
@@ -18,9 +24,16 @@ export const todosSlice = createSlice({
       const id = action.payload
       const filtered = state.items.filter(item => item.id !== id)
       state.items = filtered
+    },
+    changeActiveFiltered: (state, action) => {
+      state.activeFilter = action.payload
+    },
+    clearCompleted: (state) => {
+      const filter = state.items.filter(item => item.completed === false)
+      state.items = filter
     }
   }
 })
 
-export const { addTodo, toggle, destroy } = todosSlice.actions
+export const { addTodo, toggle, destroy, changeActiveFiltered, clearCompleted } = todosSlice.actions
 export default todosSlice.reducer
