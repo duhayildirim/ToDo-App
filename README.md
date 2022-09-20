b8288176dd7f5b73d8028716af57102541e1a084
Duha YILDIRIM <1duhayildirim@gmail.com>
2022-09-20 20:56:47 +0300

    components

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## coded with react.js
 
+*i coded in single .js file without using component*
+
 ![TO-DO-APP](/public/readme.png)
--- src/App.js
+++ src/App.js
@@ -1,76 +1,17 @@
 import React from 'react';
 import './App.css';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <React.Fragment>
       <section class="todoapp">
-        <header class="header">
-          <h1>todos</h1>
-          <form>
-            <input class="new-todo" placeholder="What needs to be done?" autoFocus />
-          </form>
-        </header>
-
-        <section class="main">
-          <input class="toggle-all" type="checkbox" />
-          <label for="toggle-all">
-            Mark all as complete
-          </label>
-
-          <ul class="todo-list">
-            <li class="completed">
-              <div class="view">
-                <input class="toggle" type="checkbox" />
-                <label>Learn JavaScript</label>
-                <button class="destroy"></button>
-              </div>
-            </li>
-            <li>
-              <div class="view">
-                <input class="toggle" type="checkbox" />
-                <label>Learn React</label>
-                <button class="destroy"></button>
-              </div>
-            </li>
-            <li>
-              <div class="view">
-                <input class="toggle" type="checkbox" />
-                <label>Have a life!</label>
-                <button class="destroy"></button>
-              </div>
-            </li>
-          </ul>
-        </section>
-
-        <footer class="footer">
-          <span class="todo-count">
-            <strong>2</strong>
-            items left
-          </span>
-
-          <ul class="filters">
-            <li>
-              <a href="#/" class="selected">All</a>
-            </li>
-            <li>
-              <a href="#/">Active</a>
-            </li>
-            <li>
-              <a href="#/">Completed</a>
-            </li>
-          </ul>
-
-          <button class="clear-completed">
-            Clear completed
-          </button>
-        </footer>
+        <Header />
+        <Content />
       </section>
-
-      <footer class="info">
-        <p>Created by <a href="https://d12n.me/">Duha Yıldırım</a></p>
-        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-      </footer>
+      <Footer />
     </React.Fragment>
   )
 }
